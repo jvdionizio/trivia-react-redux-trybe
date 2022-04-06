9543707ac060536382671277691d9d3c64e690a4
Eduardo Muchak <eduardomuchak@gmail.com>
2022-04-06 11:14:06 -0300

    feat: Requisito 5 e 6 finalizados!

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,30 +1,85 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Loading from './Loading';
 
 class Question extends Component {
   constructor() {
     super();
 
     this.state = {
-      answers: [0, 1, 2, 3],
+      correctAnswer: '',
+      answers: [],
+      loading: true,
     };
+  }
+
+  componentDidMount() {
+    const { incorrectAnswersGame, correctAnswerGame } = this.props;
+    this.setState({
+      correctAnswer: correctAnswerGame,
+      answers: [...incorrectAnswersGame, correctAnswerGame],
+    }, () => this.handleQuestions());
+  }
+
+  handleQuestions = () => {
+    const { answers } = this.state;
+    const random = this.shuffleAnswers(answers);
+    this.setState({
+      answers: random,
+      loading: false,
+    });
+  }
+
+  // Função para ramdomizar o array de respostas baseado no algoritimo de Fisher–Yates shuffle
+  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  shuffleAnswers = (array) => {
+    let currentIndex = array.length; let
+      randomIndex;
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+
+  isCorrect = (answer) => {
+    const { correctAnswer, answers } = this.state;
+    const wrongAnswers = answers.filter((a) => a !== correctAnswer);
+    if (correctAnswer === answer) return 'correct-answer';
+    return (`wrong-answer-${wrongAnswers.indexOf(answer)}`);
   }
 
   render() {
     const { results, currentQuestion } = this.props;
+    const { loading, answers } = this.state;
     return (
       <div>
-        <h2 data-testid="question-category">{results[currentQuestion].category}</h2>
-        <p data-testid="question-text">
-          {results[currentQuestion].question}
-        </p>
-        <div>
-          <button type="button">Teste</button>
-          <button type="button">Teste2</button>
-          <button type="button">Teste3</button>
-          <button type="button">Teste4</button>
-        </div>
+        {loading === true
+          ? <Loading />
+          : (
+            <div>
+              <h2 data-testid="question-category">{results[currentQuestion].category}</h2>
+              <p data-testid="question-text">
+                {results[currentQuestion].question}
+              </p>
+              <div data-testid="answer-options">
+                {answers.map((answer, index) => (
+                  <button
+                    type="button"
+                    key={ index }
+                    data-testid={ this.isCorrect(answer) }
+                  >
+                    {answer}
+                  </button>))}
+              </div>
+            </div>
+          )}
       </div>
     );
   }
@@ -33,6 +88,8 @@ class Question extends Component {
 const mapStateToProps = (state) => ({
   results: state.game.results,
   currentQuestion: state.game.currentQuestion,
+  correctAnswerGame: state.game.results[0].correct_answer,
+  incorrectAnswersGame: state.game.results[0].incorrect_answers,
 });
 
 Question.propTypes = {
