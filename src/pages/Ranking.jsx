def70d20dea764affc94fe6a2bb68bb7c9aa8f9f
jvdionizio <dionizio145@gmail.com>
2022-04-08 09:56:04 -0300

    requisito 17 finalizado

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { newGame } from '../Redux/Actions';
 
 class Ranking extends Component {
   render() {
+    const { playAgain } = this.props;
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <div>
+          <Link to="/">
+            <button
+              type="button"
+              data-testid="btn-go-home"
+              onClick={ playAgain }
+            >
+              Play Again
+            </button>
+          </Link>
+        </div>
       </div>
     );
   }
 }
 
-export default Ranking;
+const mapDispatchToProps = (dispatch) => ({
+  playAgain: () => dispatch(newGame()),
+});
+
+Ranking.propTypes = {
+  playAgain: PropTypes.func,
+}.isRequired;
+
+export default connect(null, mapDispatchToProps)(Ranking);
